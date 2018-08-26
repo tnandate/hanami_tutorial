1c0d0c692820f64853cab7d650e89bdd55a69046
tnandate <nandate.takuya@moneyforward.co.jp>
2018-08-26 21:33:45 +0900

    Displaying Validation Errors

--- spec/web/features/add_book_spec.rb
+++ spec/web/features/add_book_spec.rb
@@ -9,13 +9,13 @@ describe 'Add a book' do
     visit '/books/new'
 
     within 'form#book-form' do
-      fill_in 'Title',  with: 'New book'
-      fill_in 'Author', with: 'Some author'
-
       click_button 'Create'
     end
 
     current_path.must_equal('/books')
-    assert page.has_content?('New book')
+
+    assert page.has_content?('There was a problem with your submission')
+    assert page.has_content?('Title must be filled')
+    assert page.has_content?('Author must be filled')
   end
 end
