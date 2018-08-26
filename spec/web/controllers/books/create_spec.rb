840046c5eb75a3758ca710d40319e7e6b41a9907
tnandate <nandate.takuya@moneyforward.co.jp>
2018-08-26 20:53:47 +0900

    add controllers/books/create_spec.rb

--- spec/web/controllers/books/create_spec.rb
+++ spec/web/controllers/books/create_spec.rb
@@ -2,10 +2,25 @@ require_relative '../../../spec_helper'
 
 describe Web::Controllers::Books::Create do
   let(:action) { Web::Controllers::Books::Create.new }
-  let(:params) { Hash[] }
+  let(:params) { Hash[book: { title: 'Confident Ruby', author: 'Avdi Grimm'}] }
+  let(:repository) { BookRepository.new }
 
-  it 'is successful' do
+  before do
+    repository.clear
+  end
+
+  it 'creates a new book' do
+    action.call(params)
+    book = repository.last
+
+    book.id.wont_be_nil
+    book.title.must_equal.dig(:book, :title)
+  end
+
+  it 'redirects the user to the books listing' do
     response = action.call(params)
-    response[0].must_equal 200
+
+    response[0].must_equal 302
+    response[1]['Location'].must_equal '/books'
   end
 end
